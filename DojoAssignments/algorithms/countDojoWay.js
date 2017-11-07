d3da698e80d84b5ba504383c075bc1a747f0d1d0
Nicci Abueg <niccithenomad@Niccis-MacBook-Pro.local>
2017-11-07 11:23:42 -0600

    add python

---
+++ DojoAssignments/algorithms/countDojoWay.js
@@ -0,0 +1,14 @@
+function dojocount(start, end, multi){
+    for(var start=1; end<=100; start++){
+        console.log(start);
+        while(start<=end){
+            if(start%5===0){
+                console.log("Coding");
+                    if(start%10===0){
+                        console.log("Dojo");
+                    }
+            }
+        }
+    }
+}
+dojocount(1,100,5);
---
+++ DojoAssignments/algorithms/settings.js
@@ -0,0 +1,10 @@
+
+
+function swap(var1, var2){
+    var temp=var1;
+    var1=var2;
+    var2=temp;
+console.log("var1", var1);
+console.log("var2", var2);
+}
+swap(42, "ollie");
