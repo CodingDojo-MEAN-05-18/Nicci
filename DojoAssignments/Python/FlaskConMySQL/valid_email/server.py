0ead8a25e52b1f4ee4c243dea5a38ce3e595f2ef
Nicci Abueg <niccithenomad@Niccis-MacBook-Pro.local>
2018-01-09 09:27:03 -0600

    adding projects

--- DojoAssignments/Python/FlaskConMySQL/valid_email/server.py
+++ DojoAssignments/Python/FlaskConMySQL/valid_email/server.py
@@ -2,6 +2,10 @@ from flask import Flask, request, redirect, render_template, session, flash
 from mysqlconnection import MySQLConnector
 app = Flask(__name__)
 app.secret_key = "KeepItSimpleStupid"
+
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9\.\+_-]+@[a-zA-Z0-9\._-]+\.[a-zA-Z]*$')
+
 mysql = MySQLConnector(app,'valid_email')
 @app.route('/')
 def index():
@@ -12,23 +16,27 @@ def index():
 
 @app.route('/email_check', methods=['POST'])
 def email_check():
+    email = request.form['email']
+    valid = True
     
-    if len(request.form['email']) < 5:
-        print "got into if statement" * 4
-        flash("Please enter a valid email address.") 
-    else:
-        print "got into else statement" * 4
-        flash('Need to redirect to valid.html')
+    if email == '' or len(email) < 5:
+        valid = False
+    if len(request.form['email']) < 1 or not EMAIL_REGEX.match(request.form['email']):
+        valid = False     
+    if valid == True:
         query = "INSERT INTO users (email, created_at, updated_at) VALUE (:email, NOW(), NOW())"
         data = {
             'email': request.form['email']
         }
-        mysql.query_db(query, data)    
-    return redirect('/valid')
+        mysql.query_db(query, data)
+        flash("Thank you for the valid email: " + request.form['email'] + "!")    
+        return redirect('/valid')
+    elif valid == False:
+        flash("email can not be blank and must be valid")
+        return redirect('/')          
 
 @app.route('/valid')
 def valid_user():
-    print '%'*15,"in Valid"
     query = "SELECT * FROM users"
     users = mysql.query_db(query)
     return render_template('valid.html', all_users=users)
@@ -37,7 +45,7 @@ def valid_user():
 def delete_record():
     query = "DELETE FROM users WHERE id = :id"
     data = {
-                'id': user_id
+                'id': request.form['delete_user']
             }    
     mysql.query_db(query, data)
     return redirect('/')
