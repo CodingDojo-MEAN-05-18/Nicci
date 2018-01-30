4edc2ddf9ab75c8705ca7e071cac4f787ec684c4
Nicci Abueg <niccithenomad@Niccis-MacBook-Pro.local>
2018-01-29 20:40:29 -0600

    more fun stuff

--- DojoAssignments/Python/pythonDjango/classExample/week6_2/apps/users/urls.py
+++ DojoAssignments/Python/pythonDjango/classExample/week6_2/apps/users/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.land),
+    url(r'^users/new$', views.new),
     url(r'^users$', views.index),
-    url(r'^', views.provide_route)
+    url(r'^users/(?P<user_id>\d+)/delete$', views.delete),
+    url(r'^users/(?P<user_id>\d+)/edit$', views.edit),
+    url(r'^users/(?P<user_id>\d+)', views.show_or_update),
 ]
--- DojoAssignments/Python/pythonDjango/classExample/week6_2/week6_2/urls.py
+++ DojoAssignments/Python/pythonDjango/classExample/week6_2/week6_2/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('apps.users.urls')),
+    url(r'^surveys/', include('apps.surverys.urls'))
 ]
