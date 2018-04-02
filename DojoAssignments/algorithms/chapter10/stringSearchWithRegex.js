64bf5c745a650a5232fd26560dc1a07fbbce9f31
Nicci Lee Abueg <nicci.abueg@gmail.com>
2018-04-01 22:29:46 -0600

    updates

--- DojoAssignments/algorithms/chapter10/stringSearchWithRegex.js
+++ DojoAssignments/algorithms/chapter10/stringSearchWithRegex.js
@@ -26,26 +26,26 @@ searchPlay = '\\d';
 globalPlay = 'g'; // options are either '' or 'g'
 console.log(stringSearchRegex(primaryPlay, searchPlay, globalPlay));
 
-primary1 = 'dude';
+primary1 = 'dude dude dude';
 search1 = '[q-z]';
 global1 = 'g'; // options are either '' or 'g'
 console.log(stringSearchRegex(primary1, search1, global1));
 // ("[q-z]") --> 1; find any characters between q - z;
 
-primary2 = 'dude';
+primary2 = 'dude dude dude ';
 search2 = '(ud)';
 global2 = 'g'; // options are either '' or 'g'
 console.log(stringSearchRegex(primary2, search2, global2));
 // ("(ud)") --> 1; find the matches of the interrogation
 
-primary3 = 'dude';
+primary3 = 'dudedudedude';
 search3 = '[^a-e]';
 global3 = 'g'; // options are either '' or 'g'
 console.log(stringSearchRegex(primary3, search3, global3));
 // ("[^a-e]") --> 1; find the matches of the interrogation
 
-primary4 = 'dude';
+primary4 = 'dude dad failed';
 search4 = 'd$';
 global4 = 'g'; // options are either '' or 'g'
 console.log(stringSearchRegex(primary4, search4, global4));
-// ("d$") --> 1; find the matches of the interrogation
+// ("d$") --> -1; find the matches of the interrogation
